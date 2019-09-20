856f8b04ea6d08ab7127c68731f2244677f27533
Andrew <lyfeshomebrew@gmail.com>
2019-09-20 14:09:28 -0700

    Add more cli funtionality to editor script

--- mass_package_info_editor.py
+++ mass_package_info_editor.py
@@ -1,18 +1,35 @@
 import os, sys
+from appstore import appstore_handler
+store_handler = appstore_handler()
 
-KEY = ""
-VALUE = ""
+USAGE = "python [Root dir of SD (NOT .get/packages!)] [key to add or change across all packages] [value to set]"
 
+def exit(reason):
+    sys.exit("{} Usage - {}".format(reason,USAGE))
 
 #Passed target path
-rootpath = sys.argv[1]
+try:
+    rootpath = sys.argv[1]
+except IndexError:
+    exit("SD root path not passed.")
+if not rootpath:
+    exit("SD root path is empty.")
 
-from appstore import appstore_handler
+try:
+    key = sys.argv[2]
+except IndexError:
+    exit("key not passed.")
+if not key:
+    sys.exit("key is empty.")
 
-store_handler = appstore_handler()
+try:
+    value = sys.argv[3]
+except IndexError:
+    sys.exit("value not passed.")
+
 store_handler.set_path(rootpath)
 
 packages = store_handler.get_packages()
 
 for package in packages:
-    store_handler.edit_info(package, KEY, VALUE)
+    store_handler.edit_info(package, key, value)
